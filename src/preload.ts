f1fbd6067f4653317fa2e19688d2d19a0b8ab7f8
jessy-bgl <>
2022-11-26 21:10:39 +0100

    electron-store added as a database

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,14 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+import { contextBridge, ipcRenderer } from "electron";
+import { Heading } from "./models/heading";
+
+export const api = {
+  getHeadings: (): Promise<Heading[]> =>
+    ipcRenderer.invoke("getStoreValue", "headings"),
+  setHeadings: (value: Heading[]): Promise<void> =>
+    ipcRenderer.invoke("setStoreValue", "headings", value),
+};
+
+contextBridge.exposeInMainWorld("electron", { api });
